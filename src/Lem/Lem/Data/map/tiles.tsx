630c8ae361f352631f4ebaea5d1bc5152e8e3d1f
xdrie <xdrie@alt.icu>
2020-12-30 21:19:47 -0800

    add map

---
+++ src/Lem/Lem/Data/map/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="8" tileheight="8" tilecount="720" columns="18">
+ <image source="tiles.png" width="144" height="320"/>
+</tileset>
---
+++ src/Lem/Lem/Scenes/LocalPlayScene.cs
@@ -0,0 +1,5 @@
+namespace Lem.Scenes {
+    public class LocalPlayScene {
+        
+    }
+}
