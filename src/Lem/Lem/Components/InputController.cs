d0ab383ee0064809aee30013258266e59ffcca53
xdrie <xdrie@alt.icu>
2020-12-31 14:00:37 -0800

    gamepad support

--- src/Lem/Lem/Components/InputController.cs
+++ src/Lem/Lem/Components/InputController.cs
@@ -11,8 +11,13 @@ namespace Lem.Components {
         public override void Initialize() {
             base.Initialize();
 
+            // kbd 0
             move.AddKeyboardKeys(VirtualInput.OverlapBehavior.CancelOut, Keys.Left, Keys.Right, Keys.Up, Keys.Down);
             jump.AddKeyboardKey(Keys.Z);
+            
+            // pad 0
+            move.AddGamePadLeftStick(0);
+            jump.AddGamePadButton(0, Buttons.A);
         }
     }
 }
